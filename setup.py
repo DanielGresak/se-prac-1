8d49f02b6a2c3ac77e50e06a60d6984fd0e4ea7f
DanielGresak <80643624+DanielGresak@users.noreply.github.com>
2022-01-25 10:45:27 +0000

    second commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='sys_info',
+      version='0.1',
+      description='System info',
+      url='C:\Users\danie\Desktop\Semester-2\Software Engineering\prac1\sysinfo',
+      author='Daniel',
+      author_email='flyingcircus@example.com',
+      license='Hola',
+      packages=['sysinfo'],
+      zip_safe=False)
